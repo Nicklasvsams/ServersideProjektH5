ae24cdf022a71752f678f24b059dfce9790f8906
nicklasvsams <nicklasvsams@gmail.com>
2023-03-02 11:20:00 +0100

    Created a model for todo lists

---
+++ Models/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServersideProjektH5.Models
+{
+    public class ToDoModel
+    {
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
