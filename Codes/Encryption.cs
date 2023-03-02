7ece8d6608c0a9572605d9b94e5df5894ee23f9d
nicklasvsams <nicklasvsams@gmail.com>
2023-03-02 10:11:51 +0100

    Created encryption class

---
+++ Codes/Encryption.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace ServersideProjektH5.Codes
+{
+    public class Encryption
+    {
+        private readonly IDataProtector _dataProtector;
+
+        public Encryption(IDataProtectionProvider dataProtector)
+        {
+            _dataProtector = dataProtector.CreateProtector("ServersideProjektH5.codes.Encryption.Nicklas");
+        }
+
+        public string Protect(string valueToEncrypt) => _dataProtector.Protect(valueToEncrypt);
+
+        public string Unprotect(string valueToDecrypt) => _dataProtector.Unprotect(valueToDecrypt);
+    }
+}
